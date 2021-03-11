cda235aa41abd1accec5fd774b0c231b078826d9
Yassine Sebri <yassine.sebri@enetcom.u-sfax.tn>
2021-03-11 05:11:23 -0600

    update

--- test.c
+++ test.c
@@ -7,6 +7,9 @@
 #include <stdio.h>	   // printf
 #include <sys/types.h> // pid_t
 #include <sys/mount.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #define STACK 1024 * 1024
 
 void print_err(char const *const reason)
@@ -16,30 +19,38 @@ void print_err(char const *const reason)
 
 int shell()
 {
+	char *hostname = "container";
+	if (mount("root mount", "root mount", NULL,
+			  MS_REC | MS_BIND | MS_PRIVATE, NULL) != 0)
+	{
+		print_err("mounting proc");
+		return 1;
+	}
+	mkdir("root mount/old", 0755);
+	syscall(SYS_pivot_root, "root mount", "root mount/old");
+	chdir("/");
 	if (mount("proc", "/proc", "proc", 0, "") != 0)
 	{
 		print_err("mounting proc");
 		return 1;
 	}
-	system("/bin/zsh");
+	umount2("/old", MNT_DETACH);
+	sethostname(hostname, strlen(hostname));
+	system("/bin/sh");
 	return 0;
 }
 
 int main()
 {
+	int flags = CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWUSER | CLONE_NEWUTS | CLONE_VFORK;
 	void *stack;
 	stack = malloc(STACK);
-	pid_t pid = clone(shell, (char *)stack + STACK, CLONE_NEWPID | CLONE_NEWNS | CLONE_VFORK, 0);
+	pid_t pid = clone(shell, (char *)stack + STACK, flags, 0);
 	if (pid < 0)
 	{
 		print_err("calling clone");
 		return 1;
 	}
 	free(stack);
-	if (mount("proc", "/proc", "proc", 0, "") != 0)
-	{
-		print_err("mounting proc");
-		return 1;
-	}
 	return 0;
 }
